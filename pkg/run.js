8f4bd9617ad0a92229a78a973d456d5274e1f21c
vloe <vloe.dev@gmail.com>
2024-12-23 00:48:34 +0100

    fix ci

--- pkg/run.js
+++ pkg/run.js
@@ -1,6 +1,46 @@
 #!/usr/bin/env node
+const os = require("os")
 const { execFileSync } = require("child_process")
 const path = require("path")
 
-const binaryPath = path.join(__dirname, "voulr")
-execFileSync(binaryPath, process.argv.slice(2), { stdio: "inherit" })
+const platforms = [
+	{
+		type: "Windows_NT",
+		arch: "x64",
+		file: "win64.exe",
+	},
+	{
+		type: "Windows_NT",
+		arch: "ia32",
+		file: "win32.exe",
+	},
+	{
+		type: "Linux",
+		arch: "x64",
+		file: "linux",
+	},
+	{
+		type: "Darwin",
+		arch: "x64",
+		file: "macos",
+	},
+	{
+		type: "Darwin",
+		arch: "arm64",
+		file: "macos-arm64",
+	},
+]
+
+async function run() {
+	const type = os.type()
+	const arch = os.arch()
+	const supported = platforms.find((p) => p.type === type && p.arch === arch)
+	if (!supported) {
+		throw new Error(`unsupported platform: ${type} ${arch}`)
+	}
+
+	const binaryPath = path.join(__dirname, `bin/voulr-${supported.file}`)
+	execFileSync(binaryPath, process.argv.slice(2), { stdio: "inherit" })
+}
+
+await run()
