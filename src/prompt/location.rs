5cdeea1fb0a1d45fe1fb443d27a6027e3aa55af9
vloe <vloe.dev@gmail.com>
2024-12-26 00:24:57 +0100

    added validation

--- src/prompt/location.rs
+++ src/prompt/location.rs
@@ -1,13 +1,28 @@
-use std::{ffi::OsStr, path::PathBuf};
-
 use crate::utils::DEFAULT_LOCATION;
 use anyhow::{bail, Result};
-use inquire::{ui::RenderConfig, Text};
+use inquire::{ui::RenderConfig, validator::Validation, Text};
+use std::{ffi::OsStr, path::PathBuf};
 
 pub fn prompt(rcfg: &RenderConfig) -> Result<PathBuf> {
     let location = Text::new("Where would you like your project to be created?")
         .with_default(DEFAULT_LOCATION)
         .with_render_config(*rcfg)
+        .with_validator(|text: &str| match get_path(text) {
+            Ok(path) => {
+                let file_name = get_file_name(&path);
+                if !file_name
+                    .chars()
+                    .all(|c| c.is_ascii_lowercase() || c.is_ascii_digit() || c == '-' || c == '_')
+                {
+                    Ok(Validation::Invalid(
+					    format!("{} must only contain lowercase alphanumeric characters, dashes, and underscores", file_name).into(),
+                    ))
+                } else {
+                    Ok(Validation::Valid)
+                }
+            }
+            Err(e) => Ok(Validation::Invalid(e.into())),
+        })
         .prompt()?;
 
     let path = get_path(&location)?;
