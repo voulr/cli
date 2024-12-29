b2b9a5aa1ee12f44903b969c5ea9a2f9af4cc9ff
vloe <vloe.dev@gmail.com>
2024-12-29 14:52:46 +0100

    small fix

--- templates/rust/axum/src/main.rs
+++ templates/rust/axum/src/main.rs
@@ -3,7 +3,7 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Voulr server!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
